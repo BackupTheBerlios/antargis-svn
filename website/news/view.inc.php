9c5184c920c11fb47e08bbe5a355733bf705661d
davidkamphausen <davidkamphausen@c5f661ee-f2e3-0310-b139-8ffc597fac0b>
2005-04-12 09:15:14 +0000

    news update

--- website/news/view.inc.php
+++ website/news/view.inc.php
@@ -8,6 +8,7 @@ function news($date,$title,$body="")
 function view()
 {
 	$b=h1("News");
+	$b.=news("2005-04-12","Homepage update","Martin Kamphausen provided some nice logo. AntargisGUI-lib is proceeding - XML-support for layouting is included now.");
 	$b.=news("2005-04-05","Homepage setup","Welcome to to first version of this website!<br>There is nothing much here at the moment, but it is soon to come.");
 
 
