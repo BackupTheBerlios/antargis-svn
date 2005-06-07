84f116efdd8faa63245505a524dda79517a42590
davidkamphausen <davidkamphausen@c5f661ee-f2e3-0310-b139-8ffc597fac0b>
2005-06-07 15:01:23 +0000

    added some pics.

--- antargis/trunk/graphics/fromBlenderOutput.sh
+++ antargis/trunk/graphics/fromBlenderOutput.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-; scm file must be copied!!!
+# scm file must be copied!!!
 
 for i in 1 2 3 4 5 6 7 8 ; {
 	s=$(echo "/tmp/direction${i}_000$i.png")
-	d=$(echo "man$i.png")
+	d=$(echo "hero$i.png")
 	echo "$s $d"
 gimp --verbose -i -b "(fromBlender \"$s\" \"$d\")" '(gimp-quit 0)'
 }
