e3b416b2a563bef20434ede1eb4f8e54e4d4c541
davidkamphausen <davidkamphausen@c5f661ee-f2e3-0310-b139-8ffc597fac0b>
2005-06-07 15:35:20 +0000

    added missing files

---
+++ antargis/trunk/ant_deco.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+#
+# Copyright (c) 2005 by David Kamphausen. All rights reserved.
+#
+# ant_deco.rb
+# by David Kamphausen (david.kamphausen@web.de)
+#
+# The "Antargis" project, including all files needed to compile it,
+# is free software; you can redistribute it and/or use it and/or modify it
+# under the terms of the GNU General Public License as published
+# by the Free Software Foundation; either version 2 of the License,
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program.
+#
+
+#!/usr/bin/ruby
+
+class AntNewDeco<AntMyEntity
+	def initialize()
+		super(Pos2D.new(0,0))
+		@typeID=(rand()*12).to_i
+		setType("deco")
+		setVirtualY(100)
+	end
+	def setDecoType(t)
+		@typeID=t
+	end
+	def getTexture
+		a=["flower1a","flower2a","twig","gravel","small_bush","hole"]
+		r=(rand()*a.size).to_i
+		return a[r]
+	end
+	def xmlName
+		return "antNewDeco"
+	end
+	def saveXML(node)
+		super(node)
+		node.set("typeID",@typeID.to_s)
+	end
+	def loadXML(node)
+		super(node)
+		@typeID=node.get("typeID").to_i
+	end
+end
+
