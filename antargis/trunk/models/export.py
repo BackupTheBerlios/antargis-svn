dca537a579c7f10ab9c9b6995065e20f84d5e28c
davidkamphausen <davidkamphausen@c5f661ee-f2e3-0310-b139-8ffc597fac0b>
2005-08-22 12:07:00 +0000

    * added models

---
+++ antargis/trunk/models/export.py
@@ -0,0 +1,33 @@
+import Blender
+import struct
+from Blender import *
+from Blender.Scene import *
+
+print "Hello World: ", Blender
+
+scn = Scene.GetCurrent()
+
+filename="tower.ant2"
+
+file=open(filename,"wb")
+meshes=0
+for obj in scn.getChildren():
+	print obj.getType()
+	if obj.getType()=="Mesh":
+		meshes=meshes+1
+file.write(struct.pack("H",meshes))
+for obj in scn.getChildren():
+	print obj.getType()
+	if obj.getType()=="Mesh":
+		nmesh=obj.getData()
+		print "Mesh"
+		i=0
+		file.write(struct.pack("H",len(nmesh.faces)))
+		for face in nmesh.faces:
+			c=nmesh.materials[face.mat].rgbCol
+			file.write(struct.pack("H",len(face.v)))
+			for v in face.v:
+				file.write(struct.pack("fff",v.co[0],v.co[1],v.co[2])) # coords
+				file.write(struct.pack("fff",v.no[0],v.no[1],v.no[2])) # normal
+				file.write(struct.pack("fff",c[0],c[1],c[2])) # normal
+
