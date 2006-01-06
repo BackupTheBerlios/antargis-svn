38e0e86e5bc5fc7929ba78e91560c9eb8420383a
davidkamphausen <davidkamphausen@c5f661ee-f2e3-0310-b139-8ffc597fac0b>
2006-01-06 10:31:17 +0000

    * remove autotools

--- antargis/trunk/installdata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir ~/.Antargis
-mkdir ~/.Antargis/Antargis
-mkdir ~/.Antargis/Antargis/savegames
-mkdir ~/.Antargis/Antargis/tiles
-
-cp graphics/* ~/.Antargis/Antargis/tiles/
-cp graphics/* ~/.Antargis/Antargis/
-cp ~/.Antargis/Antargis2/savegames/savegame6.antlvl ~/.Antargis/Antargis/savegames/
