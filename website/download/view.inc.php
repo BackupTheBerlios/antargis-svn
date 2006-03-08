38144b79f68e65c54ed483dfba9c2dec429b6692
davidkamphausen <davidkamphausen@c5f661ee-f2e3-0310-b139-8ffc597fac0b>
2006-03-08 14:30:38 +0000

    * update download-page

--- website/download/view.inc.php
+++ website/download/view.inc.php
@@ -3,28 +3,38 @@
 function view()
 {
 	$b=h1("Download");
-	
-	$link=anchor("http://developer.berlios.de/svn/?group_id=3474","Berlios' SVN instructions");
-	
+
+	$version="0.1";
+
+
+
+	$b.=p("This page contains a simple instruction on how to run 'Battles of Antargis'");
+	$b.=p("You have two possibilities of getting Antargis to run. The normal way is to download it from this site:");
+	$l=anchor("http://developer.berlios.de/project/showfiles.php?group_id=3474","http://developer.berlios.de/project/showfiles.php?group_id=3474");
+	$b.=p($l,"link");
+	$b.=p("For Windows-users there is an installer.");
+	$b.=p("The other way is to get it from SVN, which is described at the bottom of this page.");
+
+	$b.=h3("Compiling");	
+	$b.=p("After you have downloaded the source-distribution you do the followin:");
+	$b.=p("tar xfz antargis-0.1.tar.gz","code");
+	$b.=p("cd antargis-0.1","code");
+	$b.=p("rake","code");
+	$b.=p("After that run the run.rb script:");
+	$b.=p("./run.rb","code");
+
+
+	$b.=h3("Get Antargis from SVN");	
+	$svn=anchor("http://developer.berlios.de/svn/?group_id=3474","Berlios' SVN instructions");
 	$b.=p("As this game is still in heavy development there are no packages or tarballs yet. But you can try the current state in svn.");
-	$b.=p("For SVN-instruction please have a look at $link");
+	$b.=p("For SVN-instruction please have a look at $svn");
 	
 	$b.=p("You have to check out 'antargis' and 'antargisgui':");
 	$b.=p("svn checkout svn://svn.berlios.de/antargis/antargisgui/trunk antargisgui","code");
 	$b.=p("svn checkout svn://svn.berlios.de/antargis/antargis/trunk antargis","code");
-	
-	$b.=p("After that you have to install the antargis-gui library with the triple:");
-	$b.=p("cd antargisgui","code");
-	$b.=p("./configure && make","code");
-	$b.=p("Get root and install by");
-	$b.=p("su -c make install","code");
-	$b.=p("The actual game, can be build with:");
-	$b.=p("cd ../antargis","code");
-	$b.=p("./configure && make","code");
-	$b.=p("After that run the antargis.rb script in the antargis directory:");
-	$b.=p("./antargis.rb","code");
-	
+
 	$b.=h3("I appreciate any helpful feedback, but please note, that this game is far from being stable or even playable.");
+
 	
 	return menuSystem($b);
 }
