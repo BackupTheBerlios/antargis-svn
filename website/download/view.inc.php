154e686efa3ee50840a560f105e728f3cc73cb59
davidkamphausen <davidkamphausen@c5f661ee-f2e3-0310-b139-8ffc597fac0b>
2006-04-01 13:22:09 +0000

    * update download-site

--- website/download/view.inc.php
+++ website/download/view.inc.php
@@ -6,15 +6,19 @@ function view()
 
 	$version="0.1";
 
+	$b.=p("Get the current release here:");
+	$l=anchor("http://developer.berlios.de/project/showfiles.php?group_id=3474","Download at berlios.de"); //http://developer.berlios.de/project/showfiles.php?group_id=3474");
+        $b.=p($l,"link");
+	$b.=p("Further instructions can be found below or in the README and INSTALL file within the package.");
 
-
+/*
 	$b.=p("This page contains a simple instruction on how to run 'Battles of Antargis'");
 	$b.=p("You have two possibilities of getting Antargis to run. The normal way is to download it from this site:");
 	$l=anchor("http://developer.berlios.de/project/showfiles.php?group_id=3474","http://developer.berlios.de/project/showfiles.php?group_id=3474");
 	$b.=p($l,"link");
 	$b.=p("For Windows-users there is an installer.");
 	$b.=p("The other way is to get it from SVN, which is described at the bottom of this page.");
-
+*/
 	$b.=h3("Compiling");	
 	$b.=p("After you have downloaded the source-distribution you do the followin:");
 	$b.=p("tar xfz antargis-0.1.tar.gz","code");
