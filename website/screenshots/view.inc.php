ceb69ad5000880b39cef60b1c1e19fb3508b499d
davidkamphausen <davidkamphausen@c5f661ee-f2e3-0310-b139-8ffc597fac0b>
2006-04-21 12:52:41 +0000

    * screenshot section changed

--- website/screenshots/view.inc.php
+++ website/screenshots/view.inc.php
@@ -3,19 +3,30 @@
 function getScreenies()
 {
 	$screenies=array(
-	array("screenies/screenshot_veryold2d","A very old screenshot. This is one of the very first attempts."),
-	"screenies/screenshot1",
-	"screenies/shot050228",
-	array("screenies/shot050304","Trees are generated and rendered with a voxel/ray-casting technique."),
-	array("screenies/050511","Old 2.5d-engine. Graphics were generated using voxels and ray-casting. I wasted a lot of time on this."),
-	"screenies/shot051024b",
-	array("screenies/shot051118","Some very old high-poly trees with apples."),
-	array("screenies/shot060222","The campaign selection screen."),
-	array("screenies/shot060222b","Some story is told..."),
-	array("screenies/shot060222c","The first tutorial island."),
-	array("screenies/shot060222d","In the north of the first tutorial Island.")
+		array("Current SVN (2006-04-21):",array(
+			array("screenies/shot060421","A hero considering to invent."),
+			array("screenies/shot060421a","Some fish."),
+			array("screenies/shot060421b","Row, row, row your boat..."),
+		)),
+		array("Before first release (pre 0.1):",array(
+			"screenies/shot051024b",
+			array("screenies/shot051118","Some very old high-poly trees with apples."),
+			array("screenies/shot060222","The campaign selection screen."),
+			array("screenies/shot060222b","Some story is told..."),
+			array("screenies/shot060222c","The first tutorial island."),
+			array("screenies/shot060222d","In the north of the first tutorial Island.")
+		)),
+		array("Old isometric engine",array(
+			"screenies/screenshot1",
+			"screenies/shot050228",
+			array("screenies/shot050304","Trees are generated and rendered with a voxel/ray-casting technique."),
+			array("screenies/050511","Old 2.5d-engine. Graphics were generated using voxels and ray-casting. I wasted a lot of time on this. The Gui engine is already usable."),
+		)),
+		array("Very old plain 2d-engine",array(
+			array("screenies/screenshot_veryold2d","A very old screenshot. This is one of the very first attempts.")
+		))
 	);
-	return array_reverse($screenies);
+	return $screenies;
 }
 
 function getDesc($s)
@@ -33,18 +44,30 @@ function smallPics()
 	$page="index.php?page=screenshots";
 	$id=0;
 	
-	foreach($screenies as $a)
+	foreach($screenies as $kate)
 	{
-		if(is_array($a))
+		$kat=$kate[0];
+		$l=$kate[1];
+		$r.="<h2>$kat</h2>\n";
+		foreach($l as $a)
 		{
-			$b=$a[0];
-			$c=$a[1];
-		}
-		else
-			$b=$a;
-		$r.=anchor($page."&screenshotid=$id",image($b."_thumb.jpg","screenshot"));
+			if(is_array($a))
+			{
+				$b=$a[0];
+				$c=$a[1];
+			}
+			else
+				$b=$a;
+			//$ref=$page."&screenshotid=$id";
+			$ref="$b.jpg";
 
-		$id++;
+			$ref="showpic.php?image=$ref&text=$c";
+			$img=image($b."_thumb.jpg","screenshot");
+			//$r.=anchor($page."&screenshotid=$id",image($b."_thumb.jpg","screenshot"));
+			$r.="<a style='text-decoration:none;' href='$ref' target='_new'>$img</a>";
+	
+			$id++;
+		}
 	}
 		
 	return div($r,"thumbs");
---
+++ website/showpic.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<center>
+<a style='text-decoration:none;color:#000000;' href='javascript:window.close()'>
+<h3 style='text-decoration:none;color:#000000;'>
+	<?=$_GET[text]?>
+</h3>
+(click on image to close window)
+<img src='<?=$_GET[image]?>'><br>
+</a>
+</center>
+</body>
+</html>
