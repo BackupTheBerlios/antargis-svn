1baf1b9798b53090e6980119a7b09dbcf677ccdd
davidkamphausen <davidkamphausen@c5f661ee-f2e3-0310-b139-8ffc597fac0b>
2006-04-21 07:35:44 +0000

    * add irc-note

--- website/contact/view.inc.php
+++ website/contact/view.inc.php
@@ -10,6 +10,9 @@ function view()
 	$b.=p("<a href='mailto:antargis-dev@lists.berlios.de'>antargis-dev@lists.berlios.de</a>");
 	$b.=p("Subscribe to list here : $list");
 
+	$b.=h3("Or in IRC:");
+	$b.=p("Go to <a href='irc://irc.freenode.net'>irc.freenode.net</a> and join channel #antargis. Might be that someone's there.");
+
 	$b.=p("&nbsp;");
 	$b.=p("Information on who the Battles of Antargis team is can be found here: <a href='http://antargis.berlios.de/wiki/index.php/People'>People</a>.");
 	$b.="<br><br>\n";
