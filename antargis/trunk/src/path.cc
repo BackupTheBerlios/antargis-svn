2f9f5309c0f989694bec82928b4e47a0bb6f82a7
davidkamphausen <davidkamphausen@c5f661ee-f2e3-0310-b139-8ffc597fac0b>
2006-07-20 06:35:48 +0000

    * added some initial construct for path-finding

---
+++ antargis/trunk/src/path.cc
@@ -0,0 +1,87 @@
+#include "path.h"
+#include <math.h>
+
+PathWeighter::~PathWeighter()
+{
+}
+float PathWeighter::weight(float h0,float h1)
+{
+  if(h0<0.3 || h1<0.3)
+    return 100000; // high weight for non-passable water ;-)
+
+  return fabs(h0-h1);
+}
+
+///////////////////////////////////////////////////////////////////////
+// Graph
+///////////////////////////////////////////////////////////////////////
+
+
+
+void SimpleGraph::addNode(const AGVector2 &p)
+{
+  Node *n=findNode(p);
+  if(!n)
+    {
+      n=new Node;
+      n->p=p;
+      mNodes.push_back(n);
+    }
+}
+void SimpleGraph::addEdge(const AGVector2 &a,const AGVector2 &b,float w0,float w1)
+{
+  Node *n1=findNode(a);
+  Node *n2=findNode(b);
+  assert(n1);
+  assert(n2);
+  Edge *e1=new Edge;
+  e1->a=n1;
+  e1->b=n2;
+  e1->w=w0;
+
+  n1->edges.push_back(e1);
+
+  Edge *e2=new Edge;
+  e2->a=n2;
+  e2->b=n1;
+  e2->w=w1;
+
+  n2->edges.push_back(e2);
+}
+
+
+SimpleGraph::~SimpleGraph()
+{
+  for(std::list<Node*>::iterator i=mNodes.begin();i!=mNodes.end();i++)
+    delete *i;
+  for(std::list<Edge*>::iterator i=mEdges.begin();i!=mEdges.end();i++)
+    delete *i;
+}
+
+SimpleGraph::Node *SimpleGraph::findNode(const AGVector2 &p)
+{
+  for(std::list<Node*>::iterator i=mNodes.begin();i!=mNodes.end();i++)
+    if((*i)->p==p)
+      return *i;
+  return 0;  
+}
+
+
+
+///////////////////////////////////////////////////////////////////////
+// Pathfinder
+///////////////////////////////////////////////////////////////////////
+
+
+Pathfinder::Pathfinder(HeightMap *pMap,PathWeighter *pWeighter):
+  mMap(pMap),
+  mWeighter(pWeighter)
+{
+}
+
+void Pathfinder::mark()
+{
+  markObject(mMap);
+  markObject(mWeighter);
+}
+
---
+++ antargis/trunk/src/path.h
@@ -0,0 +1,62 @@
+#ifndef PATH_H
+#define PATH_H
+
+#include "height_map.h"
+
+class PathWeighter:public AGRubyObject
+{
+ public:
+  virtual ~PathWeighter();
+  virtual float weight(float h0,float h1);
+};
+
+class SimpleGraph
+{
+ public:
+  struct Node;
+  struct Edge;
+  struct Edge
+  {
+    Node *a,*b;
+    float w;
+  };
+
+  struct Node
+  {
+    AGVector2 p;
+    std::list<Edge*> edges;
+  };
+
+  void addNode(const AGVector2 &p);
+  void addEdge(const AGVector2 &a,const AGVector2 &b,float w0,float w1);
+
+  ~SimpleGraph();
+
+ private:
+
+  Node *findNode(const AGVector2 &p);
+
+  // FIXME: quadtree out of nodes !!!
+
+  std::list<Node*> mNodes;
+  std::list<Edge*> mEdges;
+  
+};
+
+
+
+class Pathfinder:public AGRubyObject
+{
+ public:
+  Pathfinder(HeightMap *pMap,PathWeighter *pWeighter);
+
+  virtual void mark();
+
+  std::vector<AGVector2> computePath(const AGVector2 &from, const AGVector2 &to);
+
+ private:
+  HeightMap *mMap;
+  PathWeighter *mWeighter;
+};
+
+#endif
