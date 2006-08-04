3f7b03b80c7d7603942e2cb6cc71986185ef7450
davidkamphausen <davidkamphausen@c5f661ee-f2e3-0310-b139-8ffc597fac0b>
2006-08-04 17:36:39 +0000

    * update docs

--- antargis/trunk/src/smoke.h
+++ antargis/trunk/src/smoke.h
@@ -4,8 +4,11 @@
 #include "scenenode.h"
 #include "ag_texture.h"
 
+/// some very simple particle engine for displaying
+/// smoke and ugly fire.
 class Smoke:public SceneNode
 {
+  /// this represents one single particle
   struct Piece
   {
     AGVector3 pos;
@@ -28,11 +31,17 @@ class Smoke:public SceneNode
 
 
  public:
+  /// make some smoke (or fire) in Scene pScene with output frequency f
   Smoke(Scene *pScene,float f);
+  /// if f is true this object will display a fire - otherwise it's smoke
+  /// default is smoke
   void setFire(bool f);
   void setMaxTime(float m);
+
+  /// advance the smoke-particles; generate new ones; discard old ones
   void advance(float time);
 
+  /// let Scene take care of drawing order for transparency
   virtual bool transparent()
   {
     return true;
@@ -40,6 +49,7 @@ class Smoke:public SceneNode
 
   void draw();
 
+  /// disable the generation of new particles (smoke will fade slowly)
   void setEnabled(bool f);
 
 };
