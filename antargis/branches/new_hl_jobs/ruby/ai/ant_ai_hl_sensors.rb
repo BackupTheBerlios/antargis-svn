9832b3f9bc90480502e6a983706ed26e7b781397
davidkamphausen <davidkamphausen@c5f661ee-f2e3-0310-b139-8ffc597fac0b>
2007-05-07 19:01:26 +0000

    * added new file

---
+++ antargis/branches/new_hl_jobs/ruby/ai/ant_ai_hl_sensors.rb
@@ -0,0 +1,5 @@
+# This file implements high-level sensors. For the mathematics: These are heuristic functions
+# based on the current state of the game. They're used in the game-tree for estimating a current
+# "goodnees"-value of the game-state.
+#
+# For ease of use they'r based on low-level sensors that're implemted in ant_ai_interface.rb
