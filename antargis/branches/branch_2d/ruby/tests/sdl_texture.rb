c669b20b1e71ffd3b2dcb58cfccc6d844c60ff74
davidkamphausen <davidkamphausen@c5f661ee-f2e3-0310-b139-8ffc597fac0b>
2007-05-09 18:37:14 +0000

    * added missing files

---
+++ antargis/branches/branch_2d/ruby/tests/sdl_texture.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+
+require 'ruby/antargislib.rb'
+
+getMain.initVideo(800,600,32,false,false)
+
+
+
+class MyWidget<AGWidget
+	def initialize(p,r)
+		super
+
+a=AGSurface.new(64,64)
+p=AGPainter.new(a)
+
+p.fillRect(AGRect2.new(10,10,10,10),AGColor.new(0xFF,0,0))
+p=nil
+a.save("test0.png")
+	@t1=AGTexture.new(a)
+
+
+t=AGTexture.new(64,64)
+p=AGPainter.new(t)
+
+p.fillRect(AGRect2.new(10,10,10,10),AGColor.new(0xFF,0,0))
+p=nil
+s=AGSurface.new(t.sdlTexture)
+s.save("test1.png")
+
+
+	@t2=AGTexture.new(s)
+
+		@t3=AGTexture.new(64,64)
+		@t3.beginPaint
+		@t3.blit(@t2,AGRect2.new(0,0,64,64),AGRect2.new(0,0,64,64))
+		@t3.endPaint
+s=AGSurface.new(@t3.sdlTexture)
+s.save("test2.png")
+
+	end
+	def draw(p)
+		#p.
+		p.blit(@t1,AGRect2.new(0,0,64,64))
+		p.blit(@t2,AGRect2.new(64,0,64,64))
+		p.blit(@t3,AGRect2.new(128,0,64,64))
+	end
+end
+
+
+
+app=AGApplication.new
+
+imw=MyWidget.new(nil,AGRect2.new(0,0,800,600))
+
+app.setMainWidget(imw)
+app.run
