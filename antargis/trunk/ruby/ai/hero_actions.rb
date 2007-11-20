621d3c3c28a9134370e7e71af4a6550404e7644e
davidkamphausen <davidkamphausen@c5f661ee-f2e3-0310-b139-8ffc597fac0b>
2007-11-20 19:15:12 +0000

    * missing files...

---
+++ antargis/trunk/ruby/ai/hero_actions.rb
@@ -0,0 +1,53 @@
+module AI
+	module HeroAction
+		class Base
+			attr_reader :interface
+			def initialize(interface)
+				@interface=interface
+			end
+		end
+
+		class GetFood<Base
+			def start(hero,target)
+				puts target,target.class
+				#raise 1
+				hero.takeFood(target)
+			end
+		end
+
+		class AttackHero<Base
+			def start(hero,target)
+				assert{hero!=target}
+				hero.attack(target)
+			end
+		end
+
+		class Rest<Base
+			def start(hero)
+				hero.rest(20)
+			end
+		end
+
+		class Recruit<Base
+			def start(hero,target)
+				assert{hero!=target}
+				hero.recruit(target)
+			end
+		end
+
+		class ConstructEquipment<Base
+			def start(hero,target)
+				assert{hero!=target}
+				hero.recruit(target)
+			end
+		end
+
+		class GetEquipment<Base	
+			def start(hero,target)
+				assert{hero!=target}
+				hero.takeWeapon(target)
+			end
+		end
+
+	end
+end
---
+++ antargis/trunk/ruby/entities/ant_grave.rb
@@ -0,0 +1,25 @@
+class AntGrave<AntRubyEntity
+	TYPES=[:normal,:hero]
+
+	def initialize(map)
+		super
+		@type=:normal
+		setMesh(@type)
+		@waited=false
+	end
+	def type=(type)
+		if TYPES.member?(type)
+			@type=type
+			setMesh(@type)
+		end
+	end
+	def eventNoJob
+		if @waited
+			getMap.removeEntity(self)
+			return
+		else
+			newRestJob(40)
+			@waited=true
+		end
+	end
+end
